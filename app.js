1404314dc948774584fe6acbac774ed6e4a658f4
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-11-06 22:12:57 +0100

    upload files

---
+++ app.js
@@ -0,0 +1,21 @@
+
+const btn = document.querySelector('button');
+const ulList = document.querySelector('ul');
+
+let numberLi = 1;
+
+const addList = function () {
+    // console.log('działa');
+
+    const liElement = document.createElement('li');
+    liElement.textContent = numberLi;
+
+    if ((numberLi % 3) == 0) {
+        liElement.style.fontSize = '20px';
+    }
+
+    // console.log(liElement);
+    document.body.appendChild(liElement);
+    numberLi += 2;
+}
+btn.addEventListener('click', addList);
